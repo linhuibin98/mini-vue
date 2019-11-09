75625657cec2166fd1374d6178f2ecbb3873f356
lhb1032352370 <1032352370@qq.com>
2019-11-09 20:59:43 +0800

    '完成compile核心方法node2fragment'

--- src/vue.js
+++ src/vue.js
@@ -6,7 +6,7 @@ class Vue {
     this.$data = options.data;
 
     if (this.$el) {
-      // Compile 负责解析模板, 替换数据
+      // Compile 负责解析模板内容, 替换数据
       // 需要传入模板和数据data
       new Compile(this.$el, this);
     }
@@ -15,5 +15,20 @@ class Vue {
 
 // 解析模板
 class Compile {
+  constructor(el, vm) {
+    // el, new Vue传入的选择器, 也可以直接是dom元素
+    this.el = typeof el === 'string' ? document.querySelector(el) : el;
+    // vm, Vue的实例
+    this.vm = vm;
+
+  }
+
+  // 核心方法: 创建fragment
+  node2fragment(node) {
+    let fragment = document.createDocumentFragment();
+
+    // 将
+  }
+
 
 }
